b12b20733da502c255ef1fa9aa0e8939703342cc
eitaar <154091730+eitaar@users.noreply.github.com>
2025-09-29 14:31:20 +0100

    Add solution: javascript-29-09-25-The_Vowel_Code.js

---
+++ javascript-29-09-25-The_Vowel_Code.js
@@ -0,0 +1,7 @@
+function encode(str) {
+  return str.replace(/a/g,1).replace(/e/g,2).replace(/i/g,3).replace(/o/g,4).replace(/u/g,5);
+}
+​
+function decode(str) {
+  return str.replace(/1/g,"a").replace(/2/g,"e").replace(/3/g,"i").replace(/4/g,"o").replace(/5/g,"u");
+}
