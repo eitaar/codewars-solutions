9091d10046e1525af4b4f90289b7ce612f74f741
eitaar <154091730+eitaar@users.noreply.github.com>
2025-10-01 09:04:52 +0100

    KataSync: Updated solution for javascript-01-10-25-Convert_A_Hex_String_To_RGB.js

--- javascript-01-10-25-Convert_A_Hex_String_To_RGB.js
+++ javascript-01-10-25-Convert_A_Hex_String_To_RGB.js
@@ -1,3 +1,2 @@
-function hexStringToRGB(hexString) {
-  return {r: parseInt(hexString.slice(1).substring(0,2),16),g: parseInt(hexString.slice(1).substring(2,4),16), b: parseInt(hexString.slice(1).substring(4,6),16)}
-}
+def hex_string_to_RGB(hex_string): 
+    return {"r": int(hex_string[1:3],16), "g": int(hex_string[3:5], 16), "b": int(hex_string[5:7], 16)}
